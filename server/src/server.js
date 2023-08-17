56b4c36fd5a68f653acfaada23d608d7d33ad9f2
Tyler Jarvis <tyler.jarvis@developersinstitute.co.nz>
2023-08-18 11:32:09 +1200

    Set up the project with npm start in both front and back end folders, installed mongoose, setup docker, connected the server to MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 const port = process.env.PORT || 5001;
 const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost27017/mongo");
 
 app.listen(port, () => {
   console.log(`API server started at http://localhost:${port}`);
