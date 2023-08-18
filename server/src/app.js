e525145fbcf360d7341a83235bd1a2265bbd7e4f
Tyler Jarvis <tyler.jarvis@developersinstitute.co.nz>
2023-08-18 12:05:08 +1200

    Have setup the Auth0 side of the project - the .env files are directed towards the auth applications. Have also added the JWT to express

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+// const { auth } = require("express-oauth2-jwt-bearer");
 const app = express();
+
+// const checkJwt = auth({
+//   audience: "https://reservationizr.com",
+//   issuerBaseURL: `https://dev-qf8fljxgwjjq7gei.au.auth0.com/`,
+// });
 
 app.use(cors());
 app.use(express.json());
