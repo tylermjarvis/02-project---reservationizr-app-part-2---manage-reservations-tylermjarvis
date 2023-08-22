501b9980aec900fff51ebc3d371549f38d4775da
Tyler Jarvis <tyler.jarvis@developersinstitute.co.nz>
2023-08-22 17:25:09 +1200

    Added /restaurants and /restaurants/:id fetch requests and mapped /restaurants in the browser

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -10,9 +10,21 @@ const Restaurant = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const fetchUrl = `http://localhost:5001/restaurants/${id}`;
-      // FIXME: Make a fetch request and call setRestaurant with the response body
-      setIsLoading(false);
+      try {
+        const fetchUrl = await fetch(`http://localhost:5001/restaurants/${id}`);
+        // FIXME: Make a fetch request and call setRestaurant with the response body
+
+        // If the fecth request did not work
+        if (fetchUrl.ok === false) {
+          throw new Error("Could not fetch the restaurant.");
+        }
+
+        const data = await fetchUrl.json();
+        setRestaurant(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
     fetchData();
   }, [id]);
--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -1,9 +1,52 @@
 import "./RestaurantList.css";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const RestaurantList = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRestaurantsList = async () => {
+      try {
+        const fetchUrl = await fetch("http://localhost:5001/restaurants");
+
+        if (!fetchUrl.ok) {
+          throw new Error("Could not fetch the restarants");
+        }
+
+        const data = await fetchUrl.json();
+        setRestaurants(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchRestaurantsList();
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-      <h1>Restaurants</h1>
+      <h1 className="restaurants-heading">Restaurants</h1>
+      {restaurants.map((restaurant) => (
+        <div className="restaurant-container" key={restaurant.id}>
+          <img
+            className="restaurant-image"
+            src={restaurant.image}
+            alt={restaurant.name}
+          />
+          <h2 className="restaurant-name">{restaurant.name}</h2>
+          <p className="restaurant-description">{restaurant.description}</p>
+          <Link to={`/restaurants/${restaurant.id}`} className="reserve-button">
+            Reserve now <code>&#8594;</code>
+          </Link>
+        </div>
+      ))}
     </>
   );
 };
